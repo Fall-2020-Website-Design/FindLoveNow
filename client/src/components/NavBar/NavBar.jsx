d00987bec699f950ecf6bfd1e773fa10e010e8f3
alexzhen93 <alex_15_zhen@hotmail.com>
2020-10-12 20:31:07 -0400

    NaveBar test 2

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,40 +1,23 @@
 import React, { Component } from 'react'
-import logoImg from '../../Image/logo.svg';
+import logoImg from '../../Image/logo.svg'
 import './NavBar.css'
-import {
-    Navbar,
-    NavbarBrand,
-    Button,
-    Form,
-    FormControl
+import Navbar from 'react-bootstrap/Navbar'
+import Image from 'react-bootstrap/Image'
+import 'bootstrap/dist/css/bootstrap.css'
+import NonUserBar from './NonUserBar'
+import UserBar from './UserBar'
 
-} from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.css';
-import { Link } from 'react-router-dom';
 
 export class NavBar extends Component {
     render() {
         return (
-            <Navbar bg="light" variant="light">
-                <NavbarBrand className="mr-auto">
-                    <Link to={`/`}>
-                        <img src={logoImg} className="Love-Logo" id="FindLoveNow" alt="Find Love Logo" width={200} />
-                    </Link>
-                </NavbarBrand>
-                <Form inline>
-                    <Link to={`About`} className="l2-color mr-sm-2">
-                        About Us
-                    </Link>
-                    <Button className="b-color">
-                        <Link to={`Login`} className="l-color mr-sm-2">
-                            Login
-                        </Link>
-                         | 
-                        <Link to={`Register`} className="l-color ml-sm-2">
-                            Register
-                            </Link>
-                    </Button>
-                </Form>
+            <Navbar collapseOnSelect expand="sm" bg-="light">
+                <Navbar.Brand href={`/`}>
+                    <Image src={logoImg} className="Love-Logo" id="FindLoveNow" alt="Find Love Logo" width={200} />
+                </Navbar.Brand>
+                {
+                    <NonUserBar />
+                }
             </Navbar>
         )
     }
