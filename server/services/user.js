515eb7313f603b9eb2bc5079a35822e8396dd807
JackyLiu97 <jackyliu977@gmail.com>
2020-10-16 17:54:21 -0400

    login and backend routes finished

--- server/services/user.js
+++ server/services/user.js
@@ -1,3 +1,88 @@
 // In the services folder, we want to be able to query data from our database,
 // in this case we will be querying data from the user 
-const db = require("../models")
+const db = require("../models");
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const JWT_OPTIONS = {
+    expiresIn: 31556926,
+};
+const secretOrPrivateKey = 'secret';
+
+
+//setup for authentication
+const getJwtToken = (payload) => {
+    return new Promise((resolve, reject) => {
+        jwt.sign(payload, secretOrPrivateKey, JWT_OPTIONS, (err, token) => {
+            return err ? reject(err) : resolve(token);
+        });
+    });
+};
+
+//setup for authentication
+const verifyJwtToken = (token) => {
+    return new Promise((resolve, reject) => {
+        jwt.verify(token, secretOrPrivateKey, (err, decoded) => {
+            return err ? reject(err) : resolve(decoded);
+        });
+    });
+};
+
+
+/**
+ * 
+ * @param {String} email 
+ * @returns {Promise<Model>} User model instance  
+ */
+const findUserByEmail = async (email) => {
+    const User = await db.User.findOne({ where: { email: email } })
+    return User
+}
+
+/**
+ * 
+ * @param {String} id 
+ * @returns {Promise<Model>} User model instance  
+ */
+const findUser = async (id) => {
+    const User = await db.User.findByPk(id);
+    console.log(User + ' has been found by service');
+    return User;
+}
+
+
+/**
+ * 
+ * @param {Object} data - User data
+ * @returns {Promise<Model>} User model instance  
+ */
+const createUser = async (data) => {
+    const newUser = await db.User.create( {
+        name : data.name,
+        email : data.email, 
+        password : data.password
+    })
+    console.log(`${newUser} has been created in the function createUser`)
+    return newUser
+}
+
+/**
+ * 
+ * @param {String} password 
+ * @returns {String} hash value 
+ */
+const generateHash = async (password) => {
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+    return hash;
+};
+
+
+
+module.exports = {
+    getJwtToken,
+    verifyJwtToken,
+    findUserByEmail,
+    findUser,
+    createUser,
+    generateHash
+} 
