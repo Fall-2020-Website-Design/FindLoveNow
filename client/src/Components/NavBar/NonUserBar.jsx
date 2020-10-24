ec9545ee9c4e6caee535697ae03345ff2624101b
Alex <alex_15_zhen@hotmail.com>
2020-10-24 00:10:26 -0400

    About us page completed

--- client/src/Components/NavBar/NonUserBar.jsx
+++ client/src/Components/NavBar/NonUserBar.jsx
@@ -7,15 +7,15 @@ import './NavBar.css'
 export class NonUserBar extends Component {
     render() {
         return (
-            <Nav className="justify-content-between ">
+            <Nav fluid>
                 <Form inline >
-                    <Nav.Link className="mr-4" href={`About`} inline><div className="l2-color">About</div></Nav.Link>
+                    <Nav.Link className="mr-4" href={`About`} inline><div className="NavBar-l2-color">About</div></Nav.Link>
                 </Form>
 
-                <Form inline className="b-color">
-                    <Nav.Link href={`Login`}><div className="l-color">Login</div></Nav.Link>
-                    <div className="l-color">|</div>
-                    <Nav.Link href={`Register`}><div className="l-color">Sign Up</div></Nav.Link>
+                <Form inline className="NavBar-b-color">
+                    <Nav.Link href={`Login`}><div className="NavBar-l-color">Login</div></Nav.Link>
+                    <div className="NavBar-l-color">|</div>
+                    <Nav.Link href={`Register`}><div className="NavBar-l-color">Sign Up</div></Nav.Link>
                 </Form>
             </Nav>
         )
