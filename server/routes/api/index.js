b53b3615d23136721faadcdd8e65d27a717ae21a
JackyLiu97 <jackyliu977@gmail.com>
2020-10-25 16:42:09 -0400

    Testing SonarCloud on a PR

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -12,6 +12,10 @@ router.use('/helloworld', (req, res, next) => {
     res.status(200).send('hello world');
   });
 
+  router.use('/sonarcloud', (req, res, next) => {
+    res.status(200).send('SonarCloud PR');
+  });
+
 router.use('*', (req, res, next) => {
     res.status(404).json({ errors: [{ msg: 'Unknown API route' }] });
   });
