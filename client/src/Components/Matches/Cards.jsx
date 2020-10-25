5424a1b4ab9024d9bfe7c98709e72f9a5fc1b136
Edgar <epineda0313@gmail.com>
2020-10-24 22:43:14 -0400

    Matches.jsx has card and button components

---
+++ client/src/Components/Matches/Cards.jsx
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import Bar from './Bar';
+import Arrows from './Arrows';
+import Images from './Images';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+
+import Image from 'react-bootstrap/Image';
+import location_image from '../../Images/location.svg';
+
+export class Cards extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { 
+            currentPicture: this.props.user.currentPicture,
+            name: this.props.user.name,
+            age: this.props.user.age,
+            bio: this.props.user.bio,
+            miles: this.props.user.miles,
+            images: this.props.user.images
+        }     
+    }
+
+    handleToUpdate = (index) => {
+        this.setState({
+            currentPicture: index
+        });
+    }
+
+    nextImage = () => {
+        this.setState({
+            currentPicture: this.state.currentPicture === this.state.images.length-1 ? 0 : this.state.currentPicture + 1
+        });
+    }
+
+    prevImage = () => {
+        this.setState({
+            currentPicture: this.state.currentPicture === 0 ? this.state.images.length-1 : this.state.currentPicture - 1
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <Col className="d-inline-flex justify-content-center pt-4">
+                    <Card className="matches-card">
+                        <Card.Body className="card-body">
+                            <Bar total={this.state.images.length} current={this.state.currentPicture} click={this.handleToUpdate} />
+
+                            <Arrows prev={this.prevImage} next={this.nextImage}/>
+
+                            <Images image={this.state.images[this.state.currentPicture]} className="d-inline-flex justify-content-center " />
+
+                            <Col className="info" >
+                                <div className="name-age"> {this.state.name}, {this.state.age} </div>
+                                <div className="bio"> {this.state.bio} </div> 
+                            </Col>  
+
+                            <div className="location">
+                                <Row>
+                                    <Image alt="" src={location_image} /> {this.state.miles} miles away
+                                </Row>
+                            </div>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </div>
+        )
+    }
+}
+
+export default Cards
--- client/src/Components/Matches/Matches.jsx
+++ client/src/Components/Matches/Matches.jsx
@@ -1,19 +1,8 @@
 import React, { Component, useState, useEffect, useRef } from 'react';
-import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Image from 'react-bootstrap/Image';
-import Button from 'react-bootstrap/Button';
 import NavBar from '../NavBar/NavBar';
-import checkmark_image from '../../Images/checkmark.svg';
-import x_image from '../../Images/x.png';
-import previous_image from '../../Images/prev.svg';
-import location_image from '../../Images/location.svg';
-import sample_picture from '../../Images/samplepicture.svg'; // just a place holder for now
-import Bar from './Bar';
-import Arrows from './Arrows';
-import Images from './Images';
+import Buttons from './Buttons'
+import Cards from './Cards';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import "./Matches.css";
@@ -21,36 +10,33 @@ import "./Matches.css";
 export default class Matches extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-          currentPicture: 0,
-          name: "John",
-          age: 25,
-          bio: "Human Resources at CitiBank",
-          miles: 20,
-          images: [sample_picture,
-            'https://images.unsplash.com/photo-1449034446853-66c86144b0ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80',
-            'https://images.unsplash.com/photo-1470341223622-1019832be824?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2288&q=80',
-            'https://images.unsplash.com/photo-1448630360428-65456885c650?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2094&q=80',
-            'https://images.unsplash.com/photo-1534161308652-fdfcf10f62c4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2174&q=80']
-        };
+        this.state = { 
+        user: {
+            /* object will be loaded from db with info */
+            currentPicture: 0,
+            name: "John",
+            age: 25,
+            bio: "Human Resources at CitiBank",
+            miles: 20,
+            images: [require('../../Images/samplepicture.svg'),
+                'https://images.unsplash.com/photo-1449034446853-66c86144b0ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80',
+                'https://images.unsplash.com/photo-1470341223622-1019832be824?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2288&q=80',
+                'https://images.unsplash.com/photo-1448630360428-65456885c650?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2094&q=80',
+                'https://images.unsplash.com/photo-1534161308652-fdfcf10f62c4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2174&q=80']
+            }
+        }
     }
 
-    handleToUpdate = (index) => {
-        this.setState({
-            currentPicture:index
-        });
+    acceptUser = () => {
+        console.log("Checkmark clicked!")
     }
 
-    nextImage = () => {
-        this.setState({
-            currentPicture: this.state.currentPicture === this.state.images.length-1 ? 0 : this.state.currentPicture + 1
-        })
+    rejectedUser = () => {
+        console.log("X button clicked!")
     }
 
-    prevImage = () => {
-        this.setState({
-            currentPicture: this.state.currentPicture === 0 ? this.state.images.length-1 : this.state.currentPicture - 1
-        })
+    prevUser = () => {
+        console.log("Prev button clicked!")
     }
 
     render() {
@@ -60,50 +46,9 @@ export default class Matches extends Component {
                 
                 <Container className="card-container" fluid >
 
-                    <Col className="d-inline-flex justify-content-center pt-4">
-                        <Card className="matches-card">
-                            <Card.Body className="card-body">
-                                <Bar total={this.state.images.length} current={this.state.currentPicture} click={this.handleToUpdate} />
+                    <Cards user={this.state.user} />
 
-                                <Arrows prev={this.prevImage} next={this.nextImage}/>
-
-                                <Images image={this.state.images[this.state.currentPicture]} className="d-inline-flex justify-content-center " />
-
-                                <Col className="info" >
-                                    <div className="name-age"> {this.state.name}, {this.state.age} </div>
-                                    <div className="bio"> {this.state.bio} </div> 
-                                </Col>  
-
-                                <div className="location">
-                                    <Row>
-                                        <Image alt="" src={location_image} /> {this.state.miles} miles away
-                                    </Row>
-                                </div>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-
-                    <Col className="d-inline-flex justify-content-center">
-                        <Row className="d-inline-flex justify-content-center btn-row pt-4 pl-4">
-                            <Col>
-                                <Button variant="secondary" className="btn-circle-xl btn-xl ml-5 btn-check mr-2">
-                                    <Image alt="" src={checkmark_image} className=" align-top" fluid />
-                                </Button>
-                            </Col>
-
-                            <Col className="pt-3 pl-5">
-                                <Button variant="secondary" className="btn-circle-sm btn-sm">
-                                    <Image alt="" src={previous_image} className="align-top pt-1" fluid />
-                                </Button>
-                            </Col>
-
-                            <Col>
-                                <Button variant="secondary" className="btn-circle-xl btn-xl mr-5">
-                                    <Image alt="" src={x_image} className="align-top" fluid />
-                                </Button>
-                            </Col>
-                        </Row>
-                    </Col>
+                    <Buttons prev={this.prevUser} reject={this.rejectedUser} accept={this.acceptUser} />
                     
                 </Container>
             </div>
