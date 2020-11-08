25f5d8767f3e1e6c70944aba20d45cf6a50cb4f8
JackyLiu97 <jackyliu977@gmail.com>
2020-11-08 18:48:09 -0500

    added Messages db

---
+++ server/models/Message.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var Sequelize = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+    const Message = sequelize.define('Message', {
+        messageID: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            references: {
+                model: 'Match',
+                key: 'userID'
+            }
+        },
+        message: {
+            type: DataTypes.STRING
+        },
+        
+
+        // I don't think sender is necessary
+        // since we can get the userID of the match which will allow us to 
+        // check the sender ..... 
+        sender: {
+            type: DataTypes.STRING
+        },
+    },
+    {
+        freezeTableName: true,
+    });
+
+    
+    // Message.associate = models => {
+    //     Message.belongsTo(models.Match, { foreignKey: "matchID" })
+    // }
+
+    return Message;
+}; 
