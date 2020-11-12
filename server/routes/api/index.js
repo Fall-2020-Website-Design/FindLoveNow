8af02f227fda9d6b7dfd0a9485c369c8d12d1cda
Edgar <epineda0313@gmail.com>
2020-11-12 13:13:23 -0500

    Finished /response match route, and started /find matches route

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = new express.Router();
 const userRoutes = require('./user.js')
 const filterRoutes = require('./filter.js')
+const matchRoutes = require('./match.js')
 
 
 // sanity check to make sure routes are working properly
@@ -9,6 +10,7 @@ const filterRoutes = require('./filter.js')
 
 router.use('/users',userRoutes)
 router.use('/filter', filterRoutes)
+router.use('/match', matchRoutes)
 
 router.use('/helloworld', (req, res, next) => {
     res.status(200).send('hello world');
