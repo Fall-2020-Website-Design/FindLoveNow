84f53da84ef110b1d18f3e5cf01f71e9d8f01bca
Edgar <epineda0313@gmail.com>
2020-11-23 14:13:39 -0500

    Added route to image backend to reterive a users images in the db

--- server/routes/api/image.js
+++ server/routes/api/image.js
@@ -2,9 +2,12 @@ const express = require("express");
 const router = express.Router();
 const uploadController = require("../../controllers/image.js");
 const upload = require("../../middleware/image.js");
+const validatorErrors = require("../../middleware/validatorErrors.js");
 
 
-router.post("/upload", upload.single("file"), uploadController.uploadFiles);
+router.post("/upload/:userID", upload.single("file"), uploadController.uploadFiles);
+router.get("/get/:userID", [validatorErrors], uploadController.getUserImages);
+
 
 
 module.exports = router;
