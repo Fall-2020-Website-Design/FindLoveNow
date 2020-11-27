236ac12eaad25ae31f339edf56da1d2055ec6235
Edgar <epineda0313@gmail.com>
2020-11-27 13:07:08 -0500

    fixing code smell

--- client/src/Components/Filter/Filter.jsx
+++ client/src/Components/Filter/Filter.jsx
@@ -49,7 +49,7 @@ export default class Filter extends React.Component {
 
     handleSubmit = (e) => {
         e.preventDefault();
-        const { userID, gender, city, state, minAge, maxAge, feet, inches, alertBody } = this.state;
+        const { userID, gender, city, state, minAge, maxAge, feet, inches } = this.state;
 
         let dangerAlert = document.getElementById("filter-form-danger");
         let sucessAlert = document.getElementById("filter-form-success");
